a2a6419e9d1af92b51689242d839244b9ea8feaa
foysalahmedmin <foysalahmedmin@gmail.com>
2025-12-17 11:43:47 +0600

    Update Partnars Data

--- src/assets/data/partners.ts
+++ src/assets/data/partners.ts
@@ -5,49 +5,4 @@ export const partners = [
     name: "H&M",
     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/1200px-H%26M-Logo.svg.png",
   },
-  {
-    _id: "inditex",
-    name: "INDITEX",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Inditex_logo.svg/2560px-Inditex_logo.svg.png",
-  },
-  {
-    _id: "c-and-a",
-    name: "C&A",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0b/C%26A_logo.svg/1200px-C%26A_logo.svg.png",
-  },
-    {
-    _id: "george",
-    name: "George",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/George_Asda_logo.svg/1200px-George_Asda_logo.svg.png",
-  },
-   {
-    _id: "bestseller",
-    name: "BESTSELLER",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Bestseller_logo.svg/1200px-Bestseller_logo.svg.png",
-  },
-  {
-    _id: "soliver",
-    name: "s.Oliver",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/S.Oliver_logo.svg/1200px-S.Oliver_logo.svg.png",
-  },
-  {
-    _id: "target",
-    name: "Target",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Target_Corporation_logo_%28vector%29.svg/1200px-Target_Corporation_logo_%28vector%29.svg.png",
-  },
-  {
-    _id: "tesco",
-    name: "TESCO",
-    logo: "https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Tesco_Logo.svg/1200px-Tesco_Logo.svg.png",
-  },
-  {
-    _id: "next",
-    name: "NEXT",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Next_plc_logo.svg/1200px-Next_plc_logo.svg.png",
-  },
-   {
-    _id: "pvh",
-    name: "PVH",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/PVH_Corp_Logo.svg/1200px-PVH_Corp_Logo.svg.png",
-  }
 ];
