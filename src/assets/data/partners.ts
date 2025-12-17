a7a16d13809cdb315a092a1cfd35c6460c40c5fa
foysalahmedmin <foysalahmedmin@gmail.com>
2025-12-17 11:49:24 +0600

    -

--- src/assets/data/partners.ts
+++ src/assets/data/partners.ts
@@ -1,8 +1,62 @@
-
 export const partners = [
   {
-    _id: "hm",
-    name: "H&M",
-    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/1200px-H%26M-Logo.svg.png",
+    _id: "lidl",
+    name: "LiDL",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Lidl-Logo.svg/1200px-Lidl-Logo.svg.png",
+  },
+  {
+    _id: "4f",
+    name: "4F",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0a/4F_logo.svg/1200px-4F_logo.svg.png",
+  },
+  {
+    _id: "kik",
+    name: "KiK",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f0/KiK_Logo.svg/1200px-KiK_Logo.svg.png",
+  },
+  {
+    _id: "scanweyear-apparel",
+    name: "SCANWYEAR Apparel",
+    logo: "https://cdn.worldvectorlogo.com/logos/scanweyear.svg",
+  },
+  {
+    _id: "schiesser",
+    name: "SCHIESSER",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Schiesser_Logo.svg/1200px-Schiesser_Logo.svg.png",
+  },
+  {
+    _id: "inditex",
+    name: "INDITEX",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/Inditex_Logo.svg/1200px-Inditex_Logo.svg.png",
+  },
+  {
+    _id: "primark",
+    name: "PRIMARK",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Primark_Logo_2014.svg/1200px-Primark_Logo_2014.svg.png",
+  },
+  {
+    _id: "hugo-boss",
+    name: "BOSS Hugo Boss",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Hugo_Boss_logo.svg/1200px-Hugo_Boss_logo.svg.png",
+  },
+  {
+    _id: "rave-soul-london",
+    name: "RAVE SOUL London",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b3/Rave_Soul_London_Logo.svg/1200px-Rave_Soul_London_Logo.svg.png",
+  },
+  {
+    _id: "cleve",
+    name: "CLEVE",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Cleve_Logo.svg/1200px-Cleve_Logo.svg.png",
+  },
+  {
+    _id: "fanatics",
+    name: "Fanatics",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Fanatics_%28company%29_logo.svg/1200px-Fanatics_%28company%29_logo.svg.png",
+  },
+  {
+    _id: "tboe",
+    name: "TBOE",
+    logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/TBOE_Logo.svg/1200px-TBOE_Logo.svg.png",
   },
 ];
